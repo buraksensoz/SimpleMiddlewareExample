a77f10384b456fbb940b5d22c7418c01672b49c5
buraksensoz <buraksensoz@gmail.com>
2022-05-17 15:23:32 +0300

    Add project files.

---
+++ MiddlewareSampleProject.App/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace MiddlewareSampleProject.App.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+           
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ MiddlewareSampleProject.App/Middleware/SampleMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddlewareSampleProject.App.Middleware
+{
+    public class SampleMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SampleMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+
+        public async Task Invoke(HttpContext context) {
+            var cookieValue = context.Request.Cookies["MiddlewareCookie"];
+            var requestCount = 0;
+            try //if MiddlewareCookie isn't integer value
+            {
+                requestCount = string.IsNullOrEmpty(cookieValue) ? 0 : int.Parse(cookieValue);
+            }
+            catch { }
+            requestCount++;
+
+            context.Response.Cookies.Append("MiddlewareCookie", requestCount.ToString()) ;
+
+            await _next(context); //Go to Next Middleware
+        }
+    }
+}
